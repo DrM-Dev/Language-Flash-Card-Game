78b711de027326a97e28d90edf98fb50ed555587
Dr.M Dev <ms.dental.business@gmail.com>
2026-05-06 14:14:16 +0300

    update main + created lang_csv_reader

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ WORD_FONT = "Courier", 50, "bold"
 #
 # Buttons_FONT =
 
+#====================Globals:
+player_choice = None
+
 #====================SETUP
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 #-------------
 root = customtkinter.CTk()
-root.minsize(900,800)
-root.maxsize(900,800)
+root.minsize(900,600)
+root.maxsize(900,600)
 root.config(padx=20,pady=20)
 #-------------
 root.title("Testing ground")
@@ -34,23 +37,19 @@ widgets_y_displacement = 20
 #0000-CHECK-MARK Button
 ####-------------------------BUTTON-ART / IMAGES
 correct_b__normal_state_image = customtkinter.CTkImage(light_image=Image.open("images/right_norm.png"),size=(100, 100))
-correct_b__normal_state_image = customtkinter.CTkImage(light_image=Image.open("images/right_norm.png"),size=(100, 100))
-correct_b__normal_state_image = customtkinter.CTkImage(light_image=Image.open("images/right_norm.png"),size=(100, 100))
-#
-# correct_b__normal_state_image = ImageTk.PhotoImage(Image.open("images/right_norm.png").resize((100,100)))
-# correct_b__hover_in_image = ImageTk.PhotoImage(Image.open("images/right_hover.png").resize((100,100)))
-# correct_b__clicked_image =ImageTk.PhotoImage(Image.open("images/right_clicked.png").resize((100,100)))
-
+correct_b__hover_in_image = customtkinter.CTkImage(light_image=Image.open("images/right_hover.png"),size=(100, 100))
+correct_b__clicked_image = customtkinter.CTkImage(light_image=Image.open("images/right_clicked.png"),size=(100, 100))
 
 ####-------------------------BUTTON-MAIN-FUNCTIONS
-def button_event():
-    print("button pressed")
-
+def check_button_event():
+    global player_choice
+    player_choice = True
+    #debug
+    print("CORRECT pressed")
 
 ####-------------------------BUTTON-CONSTRUCTION Widget
-check_mark_button = customtkinter.CTkButton(root, image=correct_b__normal_state_image , text="", height=50, width=50,command=button_event, fg_color="transparent",border_width=0, hover=False)
-check_mark_button.pack(padx=20,pady=20)
-
+check_mark_button = customtkinter.CTkButton(root, image=correct_b__normal_state_image , text="", height=50, width=50,command=check_button_event, fg_color="transparent",border_width=0, hover=False)
+check_mark_button.place(x=150,y=400)
 
 ####-------------------------BUTTON-Aesthetic-functions
 #----HOVER
@@ -70,6 +69,44 @@ def check_b_unclicked(event):
 #bind events:
 check_mark_button.bind("<ButtonPress-1>", check_b_clicked)
 check_mark_button.bind("<ButtonRelease-1>", check_b_unclicked)
+
+
+#_____________________________________________________________
+#0000-WRONG-CLICK Button
+####-------------------------BUTTON-ART / IMAGES
+wrong_b__normal_state_image = customtkinter.CTkImage(light_image=Image.open("images/wrong_norm.png"),size=(100, 100))
+wrong_b__hover_in_image = customtkinter.CTkImage(light_image=Image.open("images/wrong_hover.png"),size=(100, 100))
+wrong_b__clicked_image = customtkinter.CTkImage(light_image=Image.open("images/wrong_clicked.png"),size=(100, 100))
+
+####-------------------------BUTTON-MAIN-FUNCTIONS
+def wrong_button_event():
+    global player_choice
+    player_choice = True
+    # debug
+    print("WRONG pressed")
+
+####-------------------------BUTTON-CONSTRUCTION Widget
+wrong_button = customtkinter.CTkButton(root, image=wrong_b__normal_state_image , text="", height=50, width=50,command=wrong_button_event, fg_color="transparent",border_width=0, hover=False)
+wrong_button.place(x=600,y=400)
+
+####-------------------------BUTTON-Aesthetic-functions
+#----HOVER
+def wrong_b_hover_in(event):
+    wrong_button.configure(image=wrong_b__hover_in_image)
+def wrong_b_hover_out(event):
+    wrong_button.configure(image=wrong_b__normal_state_image)
+#bind events:
+wrong_button.bind("<Enter>", wrong_b_hover_in)
+wrong_button.bind("<Leave>", wrong_b_hover_out)
+
+#----CLICK-STATE
+def wrong_b_clicked(event):
+    wrong_button.configure(image=wrong_b__clicked_image)
+def wrong_b_unclicked(event):
+    wrong_button.configure(image=wrong_b__normal_state_image)
+#bind events:
+wrong_button.bind("<ButtonPress-1>", wrong_b_clicked)
+wrong_button.bind("<ButtonRelease-1>", wrong_b_unclicked)
 
 
 #_____________________________________________________________
